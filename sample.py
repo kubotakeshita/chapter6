31ff0052761515e64e75bf2c33f1b7e8230a2ccb
kubotakeshita <s22060@tokyo.kosen-ac.jp>
2025-06-13 10:18:26 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,12 +1,15 @@
 import BME280
+import time
 
 BME280.setup()
 BME280.get_calib_param()
 
-pres = BME280.readPres()
-temp = BME280.readTemp()
-humi = BME280.readHumi()
+for i in range(10):
+        pres = BME280.readPres()
+        temp = BME280.readTemp()
+        humi = BME280.readHumi()
 
-print("pressure : %7.2f hPa" % pres)
-print("temperature : %-6.2f C" % temp)
-print("humidity : %6.2f %%" % humi)
+        print("pressure : %7.2f hPa" % pres)
+        print("temperature : %-6.2f C" % temp)
+        print("humidity : %6.2f %%" % humi)
+        time.sleep(3.0)
